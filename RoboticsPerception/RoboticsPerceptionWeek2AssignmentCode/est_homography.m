a1fc28cd9ead28efeb58f1db24e62958d82475d1
Jun Zeng <zengjunsjtu@berkeley.edu>
2018-05-09 01:57:40 -0700

    HW2

---
+++ RoboticsPerception/RoboticsPerceptionWeek2AssignmentCode/est_homography.m
@@ -0,0 +1,15 @@
+function [ H ] = est_homography(video_pts, logo_pts)
+% est_homography estimates the homography to transform each of the
+% video_pts into the logo_pts
+% Inputs:
+%     video_pts: a 4x2 matrix of corner points in the video
+%     logo_pts: a 4x2 matrix of logo points that correspond to video_pts
+% Outputs:
+%     H: a 3x3 homography matrix such that logo_pts ~ H*video_pts
+% Written for the University of Pennsylvania's Robotics:Perception course
+
+% YOUR CODE HERE
+H = [];
+
+end
+
---
+++ RoboticsPerception/RoboticsPerceptionWeek2AssignmentCode/warp_pts.m
@@ -0,0 +1,26 @@
+function [ warped_pts ] = warp_pts( video_pts, logo_pts, sample_pts)
+% warp_pts computes the homography that warps the points inside
+% video_pts to those inside logo_pts. It then uses this
+% homography to warp the points in sample_pts to points in the logo
+% image
+% Inputs:
+%     video_pts: a 4x2 matrix of (x,y) coordinates of corners in the
+%         video frame
+%     logo_pts: a 4x2 matrix of (x,y) coordinates of corners in
+%         the logo image
+%     sample_pts: a nx2 matrix of (x,y) coordinates of points in the video
+%         video that need to be warped to corresponding points in the
+%         logo image
+% Outputs:
+%     warped_pts: a nx2 matrix of (x,y) coordinates of points obtained
+%         after warping the sample_pts
+% Written for the University of Pennsylvania's Robotics:Perception course
+
+% Complete est_homography first!
+[ H ] = est_homography(video_pts, logo_pts);
+
+% YOUR CODE HERE
+
+warped_pts = [];
+end
+
