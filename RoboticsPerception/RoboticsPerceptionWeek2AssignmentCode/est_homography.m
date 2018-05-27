e1f5ef63ce0a4341d6a86f254653e4f8ecfde54b
Jun Zeng <zengjunsjtu@berkeley.edu>
2018-05-27 16:23:34 -0700

    Calculate H

--- RoboticsPerception/RoboticsPerceptionWeek2AssignmentCode/est_homography.m
+++ RoboticsPerception/RoboticsPerceptionWeek2AssignmentCode/est_homography.m
@@ -9,7 +9,16 @@ function [ H ] = est_homography(video_pts, logo_pts)
 % Written for the University of Pennsylvania's Robotics:Perception course
 
 % YOUR CODE HERE
-H = [];
+A = zeros(8,9);
+for i = 1:4
+    x = video_pts(i,:);
+    xprime = logo_pts(i,:);
+    
+    A(2*i-1,:) = [-x(1),-x(2),-1,0,0,0,x(1)*xprime(1),x(2)*xprime(1),xprime(1)];
+    A(2*i,:) = [0,0,0,-x(1),-x(2),-1,x(1)*xprime(2),x(2)*xprime(2),xprime(2)];
+end
 
+[~, ~, V] = svd(A);
+H = reshape(V(:,end), [3 3]);
 end
 
