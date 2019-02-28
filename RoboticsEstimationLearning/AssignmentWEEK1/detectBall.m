062ed72f635eb713f63547d2a0c2f8af6101ad63
Jun Zeng <zengjunsjtu@berkeley.edu>
2019-02-27 23:50:19 -0800

    Uploaded Assignment Week1

---
+++ RoboticsEstimationLearning/AssignmentWEEK1/detectBall.m
@@ -0,0 +1,48 @@
+% Robotics: Estimation and Learning 
+% WEEK 1
+% 
+% Complete this function following the instruction. 
+function [segI, loc] = detectBall(I)
+% function [segI, loc] = detectBall(I)
+%
+% INPUT
+% I       120x160x3 numerial array 
+%
+% OUTPUT
+% segI    120x160 numeric array
+% loc     1x2 or 2x1 numeric array 
+
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Hard code your learned model parameters here
+%
+% mu = 
+% sig = 
+% thre = 
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Find ball-color pixels using your model
+% 
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Do more processing to segment out the right cluster of pixels.
+% You may use the following functions.
+%   bwconncomp
+%   regionprops
+% Please see example_bw.m if you need an example code.
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Compute the location of the ball center
+%
+
+% segI = 
+% loc = 
+% 
+% Note: In this assigment, the center of the segmented ball area will be considered for grading. 
+% (You don't need to consider the whole ball shape if the ball is occluded.)
+
+end
